e0d5e40adca0880c03ade179de4373962930f0eb
moustee01@gmail.com <moustee01@gmail.com>
2023-05-13 18:43:59 +0100

    update

--- rock-paper-scissors-master/app.js
+++ rock-paper-scissors-master/app.js
@@ -7,17 +7,19 @@ const scissors = document.querySelector('.scissors')
 const triangleImage = document.querySelector('.triangle')
 const score = document.querySelector('.score')
 const statusText = document.querySelector('.status-text')
-statusText.style.display = 'none'
+//statusText.style.display = 'none'
 
 // function to randomize computer selection
 const randomChoice = function() {
   const randomIndex = Math.floor(Math.random() * choices.length)
   return choices.at(randomIndex)
 }
+
 const computerChoice = randomChoice()
 console.log(computerChoice)
 
-// compare user and computer xhice6
+
+// compare user and computer choice
 function compareChoices(user, computer) {
   if (user === computer) {
     return 'draw'
@@ -33,17 +35,24 @@ function compareChoices(user, computer) {
 
 function updateScore(winner) {
   console.log('Winner:', winner);
-  if(winner === 'user') {
+  if (winner === 'user') {
     statusText.textContent = 'You Win!'
-    statusText.style.display = 'block'
+    statusText.style.opacity = 1
+    // statusText.style.display = 'block'
     userScore += 1
-  } else if(winner === 'computer' && userScore > 0){
+  } else if (winner === 'computer' && userScore > 0) {
     statusText.textContent = 'You Lose!'
-    statusText.style.display = 'block'
+    statusText.style.opacity = 1
+    // statusText.style.display = 'block'
     userScore -= 1
-  } else if (winner === 'draw'){
+  } else if (winner === 'draw') {
     statusText.textContent = 'Draw!'
-    statusText.style.display = 'block'
+    statusText.style.opacity = 1
+    // statusText.style.display = 'block'
+  } else {
+    statusText.textContent = 'You Lose!'
+    statusText.style.opacity = 1
+    // statusText.style.display = 'block'
   }
   score.textContent = userScore
 }
@@ -66,11 +75,7 @@ function playGame() {
     updateScore(checkWinner)
     console.log(checkWinner)
   })
-  
-}
 
-//console.log(compareChoices(userChoice, computerChoice))
-//console.log(`user choice: ${userChoice}, computer choice: ${computerChoice}`)
-//console.log(rock, paper, scissors)
+}
 
 playGame()
