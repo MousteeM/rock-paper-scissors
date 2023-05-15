ebb82dae38264d865ab20646549bb7fbf5523961
moustee01@gmail.com <moustee01@gmail.com>
2023-05-15 09:56:25 +0100

    update animation

--- rock-paper-scissors-master/app.js
+++ rock-paper-scissors-master/app.js
@@ -64,16 +64,72 @@ function switchState(element) {
   triangleImage.style.opacity = 0
   playAgain.style.opacity = 1
   if (element === paper) {
-    scissors.style.display = 'none'
-    rock.style.display = 'none'
+    scissors.style.opacity = 0
+    rock.style.opacity = 0
   } else if (element === scissors) {
-    paper.style.display = 'none'
-    rock.style.display = 'none'
+    paper.style.opacity = 0
+    rock.style.opacity = 0
   } else if (element === rock) {
-    scissors.style.display = 'none'
-    paper.style.display = 'none'
+    scissors.style.opacity = 0
+    //paper.style.opacity = 0
   }
 }
+
+function moveElement(element) {
+  let position = 0;
+  let animateElement;
+
+  if (element === rock) {
+    animateElement = setInterval(animateRock, 2);
+  } else if (element === scissors) {
+    animateElement = setInterval(animateScissors, 2);
+  }
+
+  function animateScissors() {
+    if (position === 177) {
+      clearInterval(animateElement);
+    } else {
+      position++;
+      element.style.right = `${position}px`;
+    }
+  }
+
+  function animateRock() {
+    if (position === 160) {
+      clearInterval(animateElement);
+    } else {
+      position++;
+      element.style.bottom = `${position}px`;
+      element.style.right = `${position}px`;
+    }
+  }
+
+  if (element !== 'paper') {
+    // let triangle = document.getElementById('triangle');
+    // triangle.style.display = 'none';
+    element.addEventListener('click', function() {
+      if (animateElement) clearInterval(animateElement);
+      let paper = document.getElementById('paper');
+      paper.style.position = 'relative';
+      paper.style.right = `${position}px`;
+      paper.style.bottom = `${position}px`;
+    });
+  }
+}
+
+
+function getDistance(elem1, elem2) {
+  const rect1 = elem1.getBoundingClientRect();
+  const rect2 = elem2.getBoundingClientRect();
+  const distanceX = rect1.left - rect2.left;
+  const distanceY = rect1.top - rect2.top;
+
+  return Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
+};
+
+const distanceRockPaper = getDistance(rock, paper);
+const distanceRockScissors = getDistance(rock, scissors);
+const distancePaperScissors = getDistance(paper, scissors);
 
 
 function playGame() {
@@ -85,15 +141,21 @@ function playGame() {
     console.log(checkWinner)
   })
   rock.addEventListener('click', () => {
-    //moveElement(rock)
     const checkWinner = compareChoices('rock ', randomChoice())
+    const rockRect = rock.getBoundingClientRect();
+    const paperRect = paper.getBoundingClientRect();
+    const distanceX = paperRect.x - rockRect.x;
+    const distanceY = paperRect.y - rockRect.y;
+    rock.style.transition = 'transform 1s';
+    rock.style.transform = `translate(${distanceX}px, ${distanceY}px)`;
     switchState(rock)
     updateScore(checkWinner)
     console.log(checkWinner)
   })
   scissors.addEventListener('click', () => {
-    //moveElement(scissors)
     const checkWinner = compareChoices('scissors', randomChoice())
+    scissors.style.transition = 'transform 2'
+    scissors.style.translate = `${-distancePaperScissors}px`
     switchState(scissors)
     updateScore(checkWinner)
     console.log(checkWinner)
